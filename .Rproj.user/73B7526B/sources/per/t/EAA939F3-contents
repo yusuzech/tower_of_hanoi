cf7cf335f3c93d591cb658e31019e6199dc7ef2f
Yifu Yan <yanyifu94@hotmail.com>
2019-10-08 21:17:44 -0700

    push tower of hanoi demo

---
+++ .Rproj.user/73B7526B/sources/per/t/EAA939F3-contents
@@ -0,0 +1,80 @@
+library(shiny)
+library(stringr)
+library(R6)
+library(RColorBrewer)
+library(purrr)
+library(tibble)
+library(dplyr)
+library(ggplot2)
+
+source("hanoi.R")
+ui <- fluidPage(
+  sidebarLayout(
+      sidebarPanel(
+          fluidRow(
+              selectInput(
+                  inputId = "hanoi_n",
+                  label = "Number Disks",
+                  choices = 1:10,
+                  selected = 3
+              )
+          ),
+          fluidRow(
+              column(
+                  width = 6,
+                  offset = 6,
+                  actionButton("nextStep","Next",width = "100%")
+              )
+          )
+      ),
+      mainPanel(
+          plotOutput("graph"),
+          uiOutput("hint")
+      )
+          
+  )
+)
+
+server <- function(input, output, session) {
+    hanoi <- reactiveVal()
+    hanoi_result <- reactiveVal()
+    step_count <- reactiveVal()
+    observeEvent(input$hanoi_n,{
+        hanoi(toa$new(as.integer(input$hanoi_n)))
+        hanoi_result(hanoi()$steps())
+        step_count(1)
+    })
+    
+    observeEvent(input$nextStep,{
+        instruction <- hanoi_result()[step_count()]
+        from <- stringr::str_extract(instruction,"(?<=from ).(?= to)")
+        to <- stringr::str_extract(instruction,"(?<=to ).$")
+        hanoi()$piece_move_to(from,to)
+        step_count_new <- step_count() + 1
+        step_count(step_count_new)
+        
+    })
+    
+    observe({
+        req(step_count())
+        #update hints
+        output$hint <- renderUI({
+            HTML(
+                glue::glue(
+                    "<span>Step {step_count()} of {length(hanoi_result())}</span>",
+                    "<br>",
+                    "<span>Next Step:{hanoi_result()[step_count()]}</span>"
+                )
+            )
+        })
+        
+        #update graph
+        output$graph <- renderPlot(hanoi()$snapeshot())
+    })
+    
+    
+    
+    
+}
+
+shinyApp(ui, server)
---
+++ hanoi.R
@@ -0,0 +1,103 @@
+
+toa <- R6::R6Class(
+    classname = "toa",
+    public = list(
+        pieces = list(
+            "A" = list(),
+            "B" = list(),
+            "C" = list()
+        ),
+        n_piecies = 0,
+        colors = RColorBrewer::brewer.pal(10,"Set3"),
+        initialize = function(number_of_pieces){
+            stopifnot(is.integer(number_of_pieces))
+            stopifnot(number_of_pieces > 0 & number_of_pieces <= 10)
+            self$pieces$A <- purrr::map(
+                1:number_of_pieces,~list(size = .x,color = self$colors[.x])
+            ) %>% setNames(1:number_of_pieces)
+            self$n_piecies = number_of_pieces
+            invisible(self)
+        },
+        piece_move_to = function(f,t){
+            top_piece_index <- which.min(as.integer(names(self$pieces[[f]])))
+            piece <- self$pieces[[f]][top_piece_index]
+            # remove piece from f
+            self$pieces[[f]] <- self$pieces[[f]][-top_piece_index]
+            # move piece to t
+            self$pieces[[t]] <- append(self$pieces[[t]],piece)
+            invisible(self)
+        },
+        steps = function(){
+            return(
+                capture.output(
+                    private$move_via_to("A","B","C",self$n_piecies)
+                )
+            )
+        },
+        snapeshot = function(){
+            width_multiplier <- 0.04
+            state = testdf <- 
+                map2_df(self$pieces,names(self$pieces), function(pieces,name){
+                map_df(pieces,function(x){
+                    tibble::tibble(
+                        size = x$size,
+                        fill = x$color,
+                        color = "black"
+                    ) 
+                }) %>%
+                        dplyr::arrange(size) %>%
+                        dplyr::mutate(
+                            pillar = name,
+                            x = dplyr::case_when(
+                                name == "A" ~ 1,
+                                name == "B" ~ 2,
+                                name == "C" ~ 3
+                            ),
+                            y = rev(dplyr::row_number())
+                        )
+            })
+            p <- state %>%
+                ggplot() +
+                geom_segment(aes(x = 1,xend = 1, y = -0.1, yend = 10),size = 2) +
+                geom_segment(aes(x = 2,xend = 2, y = -0.1, yend = 10),size = 2) +
+                geom_segment(aes(x = 3,xend = 3, y = -0.1, yend = 10),size = 2) +
+                geom_hline(yintercept = -0.1, size = 3,) +
+                geom_text(
+                    aes(x = x, y = -1, label = labels),
+                    data = tibble::tibble(
+                        labels = c("A","B","C"),
+                        x = c(1,2,3)
+                    ),
+                    size = 8
+                ) + 
+                geom_rect(
+                    aes(
+                        xmin = x - width_multiplier * size,
+                        xmax = x + width_multiplier * size,
+                        ymin = y-1,
+                        ymax = y,
+                        fill = fill
+                    ),
+                    color = "black"
+                ) +
+                coord_fixed(ratio = 0.1) +
+                theme_void() +
+                guides(fill = FALSE)
+            return(p)
+        }
+    ),
+    private = list(
+        move_to = function(f,t){
+            cat(sprintf("move from %s to %s\n",f,t))
+        },
+        move_via_to = function(f,v,t,n){
+            if(n > 1) {
+                private$move_via_to(f,t,v,n-1)
+                private$move_to(f,t)
+                private$move_via_to(v,f,t,n-1)
+            } else {
+                private$move_to(f,t)
+            }
+        }
+    )
+)
